49a5a4ffa7ca23414954fea32083f6fd44db019a
Adrian Nalwalker <analwalker@planetfg.local>
2017-12-20 10:05:37 -0800

    initial commit basic setup

---
+++ server.js
@@ -0,0 +1,18 @@
+//require path, body-parser, and express
+var path = require('path');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+
+//set up the port
+var PORT = process.env.PORT || 3000;
+// set up middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
