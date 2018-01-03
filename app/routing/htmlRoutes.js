bb18108c593f7693cda310730ebcdc584a4bbc52
Adrian Nalwalker <analwalker@planetfg.local>
2018-01-02 16:16:09 -0800

    a smidgeon of progress

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,3 +2,10 @@
 
 
 //a default route that leads back home
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+});
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+});
--- server.js
+++ server.js
@@ -3,12 +3,16 @@ var path = require('path');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+var friends = require('./app/data/friends')
 
 //set up the port, deployable to heroku etc.
 var PORT = process.env.PORT || 3000;
 // set up middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+
+
 
 
 // Starts the server to begin listening
